cdb87c097c7b2f5cce06e3c91fb2c4d0475654f6
jmusone <jacobmusone@gmail.com>
2025-02-12 20:55:54 -0500

    one more base change attempt

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/jmusone.github.io/dist",
+  base: "/",
   test: {
     globals: true,
     environment: 'jsdom',
