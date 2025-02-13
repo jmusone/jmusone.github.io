173f4368d76d2f05a0265b556c285a3281be5b3f
jmusone <jacobmusone@gmail.com>
2025-02-12 20:45:33 -0500

    so confused why the page isn't loading

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/dist/",
+  base: "/jmusone.github.io",
   test: {
     globals: true,
     environment: 'jsdom',
