dd4ddf27c3c7646aa003ee9172f6f28c005953be
jmusone <jacobmusone@gmail.com>
2025-02-12 20:11:39 -0500

    added basepath to vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: "https://jmusone.github.io/",
   test: {
     globals: true,
     environment: 'jsdom',
