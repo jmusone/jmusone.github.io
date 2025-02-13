e5b8dc17485f5e33836764980a7700a8903e9c05
jmusone <jacobmusone@gmail.com>
2025-02-12 20:52:06 -0500

    still not sure why it's not working lol

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/jmusone.github.io",
+  base: "/jmusone.github.io/dist",
   test: {
     globals: true,
     environment: 'jsdom',
